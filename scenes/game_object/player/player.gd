eaf183f7932333d271fadfbffe5c740147af4ef8
Praveen <rpraveen.engineer@gmail.com>
2023-12-02 19:21:03 +0530

    Added health component to player

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -4,6 +4,17 @@ extends CharacterBody2D
 
 const MAX_SPEED: int = 150
 const ACCELERATION_SMOOTHING: int = 20
+@onready var collision_area: Area2D = $CollisionArea
+@onready var health_component: HealthComponent = $HealthComponent
+@onready var damager_interval_timer: Timer = $DamagerIntervalTimer
+
+var no_of_colliding_bodies: int = 0
+
+func _ready() -> void:
+	collision_area.body_entered.connect(_on_body_entered)
+	collision_area.body_exited.connect(_on_body_exited)
+	damager_interval_timer.timeout.connect(_on_damager_interval_timer_timeout)
+
 
 func _process(delta: float) -> void:
 	var movement_vector: Vector2 = get_movement_vector()
@@ -15,7 +26,29 @@ func _process(delta: float) -> void:
 	move_and_slide()
 
 
+func check_deal_damage() -> void:
+	if no_of_colliding_bodies == 0 || !damager_interval_timer.is_stopped():
+		return
+	health_component.damage(1)
+	damager_interval_timer.start()
+	print(health_component.current_health)
+
+
 func get_movement_vector() -> Vector2:
 	var x_movement: float = Input.get_axis("move_left", "move_right")
 	var y_movement: float = Input.get_axis("move_up", "move_down")
 	return Vector2(x_movement, y_movement)
+
+
+func _on_body_entered(_body: Node2D) -> void:
+	no_of_colliding_bodies += 1
+	check_deal_damage()
+
+
+func _on_body_exited(_body: Node2D) -> void:
+	no_of_colliding_bodies -= 1
+	damager_interval_timer.stop()
+
+
+func _on_damager_interval_timer_timeout() -> void:
+	check_deal_damage()
