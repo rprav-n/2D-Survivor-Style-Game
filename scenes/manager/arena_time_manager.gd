7f01d1d8672460e2e7c222381fb46dc31b3f0882
Praveen <rpraveen.engineer@gmail.com>
2023-12-02 19:50:09 +0530

    Added victory screen

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -2,9 +2,18 @@ class_name ArenaTimeManager
 
 extends Node
 
-
+@export var victory_screen_scene: PackedScene
 @onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	timer.timeout.connect(_on_timer_timeout)
 
 
 func get_time_elapsed() -> float:
 	return timer.wait_time - timer.time_left
+
+
+func _on_timer_timeout() -> void:
+	var victory_screen_instance: VictoryScreen = victory_screen_scene.instantiate() as VictoryScreen
+	add_child(victory_screen_instance)
---
+++ scenes/ui/victory_screen.gd
@@ -0,0 +1,20 @@
+class_name VictoryScreen
+
+extends CanvasLayer
+@onready var restart_button: Button = %RestartButton
+@onready var quit_button: Button = %QuitButton
+
+
+func _ready() -> void:
+	get_tree().paused = true
+	restart_button.pressed.connect(_on_restart_button_pressed)
+	quit_button.pressed.connect(_on_quit_button_pressed)
+
+
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
