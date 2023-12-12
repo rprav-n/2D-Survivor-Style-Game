d12471e9c3790fe02a68b49ab9fc5ab598aef66d
Praveen <rpraveen.engineer@gmail.com>
2023-12-12 23:37:48 +0530

    Using weighted table for enemy spawning

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -5,6 +5,7 @@ extends Node
 const SPAWN_RADIUS: int = 375
 
 @export var basic_enemy_scene: PackedScene
+@export var wizard_enemy_scene: PackedScene
 @export var difficulty_manager: DifficultyManager
 @onready var timer: Timer = $Timer
 
@@ -12,8 +13,10 @@ var player: Player = null
 var entities_layer: Node2D = null
 
 var base_spawn_time: float = 0
+var enemy_table = WeightedTable.new()
 
 func _ready() -> void:
+	enemy_table.add_item(basic_enemy_scene, 10)
 	base_spawn_time = timer.wait_time
 	entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	player = get_tree().get_first_node_in_group("player")
@@ -24,8 +27,9 @@ func _ready() -> void:
 func spawn_enemy() -> void:
 	if !is_instance_valid(entities_layer) || !is_instance_valid(player):
 		return
-
-	var enemy: CharacterBody2D = basic_enemy_scene.instantiate() as CharacterBody2D
+	
+	var enemy_scene: PackedScene = enemy_table.pick_item()
+	var enemy: CharacterBody2D = enemy_scene.instantiate() as CharacterBody2D
 	entities_layer.add_child(enemy)
 	enemy.global_position = get_spawn_position()
 
@@ -59,3 +63,6 @@ func _on_difficulty_increase(difficulty: int) -> void:
 	var time_off: float = (0.1 / 12) * difficulty
 	time_off = min(time_off, 0.7)
 	timer.wait_time = base_spawn_time - time_off
+
+	if difficulty == 6:
+		enemy_table.add_item(wizard_enemy_scene, 20)
