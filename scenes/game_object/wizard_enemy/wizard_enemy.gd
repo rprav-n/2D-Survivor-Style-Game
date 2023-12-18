df70c8c38dc171fc97fdb1220b0ff85eb617601a
Praveen <rpraveen.engineer@gmail.com>
2023-12-18 19:30:26 +0530

    Animating the wizard

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -3,11 +3,19 @@ extends CharacterBody2D
 @onready var velocity_component: VelocityComponent = $VelocityComponent
 @onready var visuals: Node2D = $Visuals
 
+var is_moving: bool = false
 
 func _process(_delta: float) -> void:
-	velocity_component.accelerate_to_player()
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else:
+		velocity_component.decelerate()
 	velocity_component.move(self)
 	
 	var move_sign: int = sign(velocity.x)
 	if move_sign != 0:
 		visuals.scale.x = move_sign
+
+
+func set_is_moving(moving: bool) -> void:
+	is_moving = moving
